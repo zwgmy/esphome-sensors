472b130016770a38691155c0441d6a15350b1cd5
zwgmy <36669321+zwgmy@users.noreply.github.com>
2025-01-20 19:30:32 +0800

    Update sensor.py

--- components/wf183de_i2c/sensor.py
+++ components/wf183de_i2c/sensor.py
@@ -6,14 +6,14 @@ from esphome.const import UNIT_CELSIUS, UNIT_HECTOPASCAL, ICON_THERMOMETER, ICON
 
 DEPENDENCIES = ['i2c']
 
-wf180_ns = cg.esphome_ns.namespace('wf180de_i2c')
-WF180Component = wf180_ns.class_('WF180Component', cg.PollingComponent, i2c.I2CDevice)
+wf183de_i2c_ns = cg.esphome_ns.namespace('wf183de_i2c')
+WF183DE_I2C_Sensor = wf183de_i2c_ns.class_('WF183DE_I2C_Sensor', cg.PollingComponent, i2c.I2CDevice)
 
-CONFIG_SCHEMA = sensor.sensor_schema(WF180Component).extend({
-    cv.GenerateID(): cv.declare_id(WF180Component),
+CONFIG_SCHEMA = sensor.sensor_schema(WF183DE_I2C_Sensor).extend({
+    cv.GenerateID(): cv.declare_id(WF183DE_I2C_Sensor),
     cv.Optional('temperature'): sensor.sensor_schema(UNIT_CELSIUS, ICON_THERMOMETER, 1),
     cv.Optional('pressure'): sensor.sensor_schema(UNIT_HECTOPASCAL, ICON_GAUGE, 1),
-}).extend(cv.COMPONENT_SCHEMA).extend(i2c.i2c_device_schema(0x76))
+}).extend(cv.COMPONENT_SCHEMA).extend(i2c.i2c_device_schema(0x6D))
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
