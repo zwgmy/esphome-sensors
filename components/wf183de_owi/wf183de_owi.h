a6523950a3323ef21b50d977a0f944f87bb5f0bd
zwgmy <36669321+zwgmy@users.noreply.github.com>
2025-01-21 20:36:37 +0800

    Update wf183de_owi.h

--- components/wf183de_owi/wf183de_owi.h
+++ components/wf183de_owi/wf183de_owi.h
@@ -9,6 +9,8 @@ namespace wf183de_owi {
 
 class WF183DE_OWI_Sensor : public PollingComponent, public one_wire::OneWireDevice {
  public:
+  WF183DE_OWI_Sensor(uint64_t address, uint32_t update_interval);
+
   void setup() override;
   void update() override;
   void dump_config() override;
@@ -16,14 +18,19 @@ class WF183DE_OWI_Sensor : public PollingComponent, public one_wire::OneWireDevi
   void set_temperature_sensor(sensor::Sensor *temperature_sensor) { temperature_sensor_ = temperature_sensor; }
   void set_pressure_sensor(sensor::Sensor *pressure_sensor) { pressure_sensor_ = pressure_sensor; }
 
+  void set_name(const std::string &name) { name_ = name; }
+  const std::string &get_name() const { return name_; }
+
  protected:
   sensor::Sensor *temperature_sensor_{nullptr};
   sensor::Sensor *pressure_sensor_{nullptr};
   uint8_t scratch_pad_[9] = {0};
+  std::string name_;
 
   /// Get the number of milliseconds we have to wait for the conversion phase.
   uint16_t millis_to_wait_for_conversion_() const;
   bool read_scratch_pad_();
+  void read_scratch_pad_int_();
   bool check_scratch_pad_();
   float get_temp_c_();
   float get_pressure_hpa_();
