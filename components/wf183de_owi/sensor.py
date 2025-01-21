0a4bbbf5a8fe09ff4a63526ad6b5aa7539f33bfb
zwgmy <36669321+zwgmy@users.noreply.github.com>
2025-01-21 13:35:16 +0800

    Update sensor.py

--- components/wf183de_owi/sensor.py
+++ components/wf183de_owi/sensor.py
@@ -1,8 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import pins
 from esphome.components import sensor, one_wire
-from esphome.const import UNIT_CELSIUS, UNIT_HECTOPASCAL, ICON_THERMOMETER, ICON_GAUGE, CONF_ID, CONF_PIN, CONF_ADDRESS
+from esphome.const import UNIT_CELSIUS, UNIT_HECTOPASCAL, ICON_THERMOMETER, ICON_GAUGE, CONF_ID, CONF_ADDRESS
 
 DEPENDENCIES = ['one_wire']
 
@@ -11,7 +10,6 @@ WF183DE_OWI_Sensor = wf183de_owi_ns.class_('WF183DE_OWI_Sensor', cg.PollingCompo
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(WF183DE_OWI_Sensor),
-    cv.Required(CONF_PIN): pins.gpio_input_pin_schema,
     cv.Required(CONF_ADDRESS): cv.hex_uint64,
     cv.Optional('temperature'): sensor.sensor_schema(unit_of_measurement=UNIT_CELSIUS, icon=ICON_THERMOMETER, accuracy_decimals=1),
     cv.Optional('pressure'): sensor.sensor_schema(unit_of_measurement=UNIT_HECTOPASCAL, icon=ICON_GAUGE, accuracy_decimals=1),
@@ -19,9 +17,8 @@ CONFIG_SCHEMA = cv.Schema({
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
-    pin = yield cg.gpio_pin_expression(config[CONF_PIN])
     address = config[CONF_ADDRESS]
-    var = cg.new_Pvariable(config[CONF_ID], config['update_interval'], pin, address)
+    var = cg.new_Pvariable(config[CONF_ID], config['update_interval'], address)
     yield cg.register_component(var, config)
     yield one_wire.register_one_wire_bus(var, config)
 
