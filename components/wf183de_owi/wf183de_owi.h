4189f3ab28cd6402d5753f69fb350615ef1f9c00
zwgmy <36669321+zwgmy@users.noreply.github.com>
2025-01-21 09:39:19 +0800

    Update wf183de_owi.h

--- components/wf183de_owi/wf183de_owi.h
+++ components/wf183de_owi/wf183de_owi.h
@@ -12,14 +12,17 @@ class WF183DE_OWI_Sensor : public PollingComponent, public dallas::DallasCompone
   sensor::Sensor *temperature_sensor;
   sensor::Sensor *pressure_sensor;
 
-  // 构造函数：传入更新间隔和 One-Wire 引脚
-  WF183DE_OWI_Sensor(uint32_t update_interval, InternalGPIOPin *pin);
+  // 构造函数：传入更新间隔、One-Wire引脚和传感器地址
+  WF183DE_OWI_Sensor(uint32_t update_interval, InternalGPIOPin *pin, uint64_t address);
 
   void setup() override;
   void update() override;
 
   void set_temperature_sensor(sensor::Sensor *sensor) { temperature_sensor = sensor; }
   void set_pressure_sensor(sensor::Sensor *sensor) { pressure_sensor = sensor; }
+
+ private:
+  uint64_t address_;
 };
 
 }  // namespace wf183de_owi
