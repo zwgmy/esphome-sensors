319887f2f24fc66c20e93609fe818aa81e9b8418
zwgmy <36669321+zwgmy@users.noreply.github.com>
2025-01-21 09:20:22 +0800

    Create wf183de_owi.h

---
+++ components/wf183de_owi/wf183de_owi.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/one_wire/one_wire.h"
+#include "esphome/components/sensor/sensor.h"
+
+namespace esphome {
+namespace wf183de_owi {
+
+class WF183DE_OWI_Sensor : public PollingComponent, public one_wire::OneWire {
+ public:
+  sensor::Sensor *temperature_sensor;
+  sensor::Sensor *pressure_sensor;
+
+  // 构造函数：传入更新间隔和 One-Wire 引脚
+  WF183DE_OWI_Sensor(uint32_t update_interval, InternalGPIOPin *pin);
+
+  void setup() override;
+  void update() override;
+
+  void set_temperature_sensor(sensor::Sensor *sensor) { temperature_sensor = sensor; }
+  void set_pressure_sensor(sensor::Sensor *sensor) { pressure_sensor = sensor; }
+};
+
+}  // namespace wf183de_owi
+}  // namespace esphome
