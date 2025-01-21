90133e49c8c0eb7c76eb45ff96408ef887164b75
zwgmy <36669321+zwgmy@users.noreply.github.com>
2025-01-21 20:36:18 +0800

    Update sensor.py

--- components/wf183de_owi/sensor.py
+++ components/wf183de_owi/sensor.py
@@ -7,19 +7,22 @@ from esphome.const import (
     STATE_CLASS_MEASUREMENT,
     UNIT_CELSIUS,
     UNIT_HECTOPASCAL,
+    CONF_ID,
+    CONF_ADDRESS,
+    CONF_UPDATE_INTERVAL
 )
 
-CONF_UPDATE_INTERVAL = "update_interval"
-CONF_ADDRESS = "address"
-
+# 定义命名空间
 wf183de_owi_ns = cg.esphome_ns.namespace("wf183de_owi")
 
+# 定义传感器类
 WF183DE_OWI_Sensor = wf183de_owi_ns.class_(
     "WF183DE_OWI_Sensor",
     cg.PollingComponent,
     one_wire.OneWireDevice,
 )
 
+# 配置模式
 CONFIG_SCHEMA = (
     cv.Schema(
         {
@@ -27,14 +30,12 @@ CONFIG_SCHEMA = (
             cv.Required(CONF_ADDRESS): cv.hex_int,
             cv.Optional(CONF_UPDATE_INTERVAL, default="60s"): cv.update_interval,
             cv.Optional("temperature"): sensor.sensor_schema(
-                WF183DE_OWI_Sensor,
                 unit_of_measurement=UNIT_CELSIUS,
                 accuracy_decimals=1,
                 device_class=DEVICE_CLASS_TEMPERATURE,
                 state_class=STATE_CLASS_MEASUREMENT,
             ),
             cv.Optional("pressure"): sensor.sensor_schema(
-                WF183DE_OWI_Sensor,
                 unit_of_measurement=UNIT_HECTOPASCAL,
                 accuracy_decimals=1,
                 device_class=DEVICE_CLASS_PRESSURE,
