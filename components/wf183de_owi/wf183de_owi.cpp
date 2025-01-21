52010b866058410624dbe4cf7a367fd8ce50390e
zwgmy <36669321+zwgmy@users.noreply.github.com>
2025-01-21 09:39:37 +0800

    Update wf183de_owi.cpp

--- components/wf183de_owi/wf183de_owi.cpp
+++ components/wf183de_owi/wf183de_owi.cpp
@@ -7,8 +7,8 @@ namespace wf183de_owi {
 
 static const char *TAG = "wf183de_owi";
 
-WF183DE_OWI_Sensor::WF183DE_OWI_Sensor(uint32_t update_interval, InternalGPIOPin *pin)
-    : PollingComponent(update_interval), DallasComponent(pin) {}
+WF183DE_OWI_Sensor::WF183DE_OWI_Sensor(uint32_t update_interval, InternalGPIOPin *pin, uint64_t address)
+    : PollingComponent(update_interval), DallasComponent(pin), address_(address) {}
 
 void WF183DE_OWI_Sensor::setup() {
   ESP_LOGCONFIG(TAG, "Setting up WF183DE OWI Sensor...");
