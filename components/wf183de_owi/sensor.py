78d304e284ccb10dce4e45eb78486cf32006f799
zwgmy <36669321+zwgmy@users.noreply.github.com>
2025-01-21 13:42:10 +0800

    Update sensor.py

--- components/wf183de_owi/sensor.py
+++ components/wf183de_owi/sensor.py
@@ -10,7 +10,7 @@ WF183DE_OWI_Sensor = wf183de_owi_ns.class_('WF183DE_OWI_Sensor', cg.PollingCompo
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(WF183DE_OWI_Sensor),
-    cv.Required(CONF_ADDRESS): cv.hex_uint64,
+    cv.Required(CONF_ADDRESS): cv.hex_int,
     cv.Optional('temperature'): sensor.sensor_schema(unit_of_measurement=UNIT_CELSIUS, icon=ICON_THERMOMETER, accuracy_decimals=1),
     cv.Optional('pressure'): sensor.sensor_schema(unit_of_measurement=UNIT_HECTOPASCAL, icon=ICON_GAUGE, accuracy_decimals=1),
     cv.Optional('update_interval', default="60s"): cv.update_interval,
