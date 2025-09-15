9ffa8fc77bcb8bd23bee1a7754852099bef4ace4
zwgmy <36669321+zwgmy@users.noreply.github.com>
2025-09-15 17:02:18 +0800

    Update sensor.py

--- components/T12/sensor.py
+++ components/T12/sensor.py
@@ -3,6 +3,8 @@ import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import (
     CONF_ID,
+    CONF_OFFSET,
+    CONF_FACTOR,
     DEVICE_CLASS_TEMPERATURE,
     STATE_CLASS_MEASUREMENT,
     UNIT_CELSIUS,
@@ -17,6 +19,9 @@ T12TemperatureSensor = t12_temperature_sensor_ns.class_(
     "T12TemperatureSensor", sensor.Sensor, cg.PollingComponent
 )
 
+CONF_OFFSET_VOLTAGE = "offset_voltage"
+CONF_AMPLIFICATION_FACTOR = "amplification_factor"
+
 CONFIG_SCHEMA = (
     sensor.sensor_schema(
         T12TemperatureSensor,
@@ -26,10 +31,19 @@ CONFIG_SCHEMA = (
         state_class=STATE_CLASS_MEASUREMENT,
         device_class=DEVICE_CLASS_TEMPERATURE,
     )
-    .extend(cv.polling_component_schema("60s"))
+    .extend(cv.polling_component_schema("100ms"))
+    .extend(
+        {
+            cv.Optional(CONF_OFFSET_VOLTAGE, default=100.0): cv.float_,
+            cv.Optional(CONF_AMPLIFICATION_FACTOR, default=200.0): cv.float_,
+        }
+    )
 )
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
+    
+    cg.add(var.set_offset_voltage(config[CONF_OFFSET_VOLTAGE]))
+    cg.add(var.set_amplification_factor(config[CONF_AMPLIFICATION_FACTOR]))
